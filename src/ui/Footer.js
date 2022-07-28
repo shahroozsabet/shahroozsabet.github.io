4be61229b4e6f7729e536e803e6355103d83aabc
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-07-28 18:15:53 +0200

    LazyLoadImage added.

--- src/ui/Footer.js
+++ src/ui/Footer.js
@@ -1,35 +1,57 @@
-import React from 'react';
+import React from "react";
 import Link from "../Link";
 import {Grid, Hidden, makeStyles} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     footer: {
-        backgroundColor: theme.palette.common.blue, width: "100%", zIndex: 1302, position: "relative"
-    }, adornment: {
-        width: "25em", verticalAlign: "bottom", [theme.breakpoints.down("md")]: {
+        backgroundColor: theme.palette.common.blue,
+        width: "100%",
+        zIndex: 1302,
+        position: "relative"
+    },
+    adornment: {
+        width: "25em",
+        verticalAlign: "bottom",
+        [theme.breakpoints.down("md")]: {
             width: "21em"
-        }, [theme.breakpoints.down("xs")]: {
+        },
+        [theme.breakpoints.down("xs")]: {
             width: "15em"
         }
-    }, mainContainer: {
+    },
+    mainContainer: {
         position: "absolute"
-    }, link: {
-        color: "white", fontFamily: "Arial", fontSize: "0.75rem", fontWeight: "bold", textDecoration: "none"
-    }, gridItem: {
+    },
+    link: {
+        color: "white",
+        fontFamily: "Arial",
+        fontSize: "0.75rem",
+        fontWeight: "bold",
+        textDecoration: "none"
+    },
+    gridItem: {
         margin: "3em"
-    }, icon: {
-        height: "4em", width: "4em", [theme.breakpoints.down("xs")]: {
-            height: "2.5em", width: "2.5em"
+    },
+    icon: {
+        height: "4em",
+        width: "4em",
+        [theme.breakpoints.down("xs")]: {
+            height: "2.5em",
+            width: "2.5em"
         }
-    }, socialContainer: {
-        position: "absolute", marginTop: "-6em", right: "1.5em", [theme.breakpoints.down("xs")]: {
+    },
+    socialContainer: {
+        position: "absolute",
+        marginTop: "-6em",
+        right: "1.5em",
+        [theme.breakpoints.down("xs")]: {
             right: "0.6em"
         }
     }
-}))
+}));
 
 export default function Footer(props) {
-    const classes = useStyles()
+    const classes = useStyles();
 
     return (<footer className={classes.footer}>
         <Hidden mdDown>
@@ -64,7 +86,7 @@ export default function Footer(props) {
                             component={Link}
                             onClick={() => {
                                 props.setValue(1);
-                                props.setSelectedIndex(0)
+                                props.setSelectedIndex(0);
                             }}
                             href={"/services"}
                             className={classes.link}
@@ -78,7 +100,7 @@ export default function Footer(props) {
                             className={classes.link}
                             onClick={() => {
                                 props.setValue(1);
-                                props.setSelectedIndex(1)
+                                props.setSelectedIndex(1);
                             }}
                         >
                             Custom Software Development
@@ -90,7 +112,7 @@ export default function Footer(props) {
                             className={classes.link}
                             onClick={() => {
                                 props.setValue(1);
-                                props.setSelectedIndex(2)
+                                props.setSelectedIndex(2);
                             }}
                         >
                             Mobile App Development
@@ -100,7 +122,7 @@ export default function Footer(props) {
                             component={Link}
                             onClick={() => {
                                 props.setValue(1);
-                                props.setSelectedIndex(3)
+                                props.setSelectedIndex(3);
                             }}
                             href={"/websites"}
                             className={classes.link}
@@ -119,36 +141,36 @@ export default function Footer(props) {
                         <Grid
                             item
                             component={Link}
-                            onClick={() => props.setValue(2)}
                             href={"/revolution"}
                             className={classes.link}
+                            onClick={() => props.setValue(2)}
                         >
                             The Revolution
                         </Grid>
                         <Grid
                             item
                             component={Link}
-                            onClick={() => props.setValue(2)}
                             href={"/revolution"}
                             className={classes.link}
+                            onClick={() => props.setValue(2)}
                         >
                             Vision
                         </Grid>
                         <Grid
                             item
                             component={Link}
-                            onClick={() => props.setValue(2)}
                             href={"/revolution"}
                             className={classes.link}
+                            onClick={() => props.setValue(2)}
                         >
                             Technology
                         </Grid>
                         <Grid
                             item
                             component={Link}
-                            onClick={() => props.setValue(2)}
                             href={"/revolution"}
                             className={classes.link}
+                            onClick={() => props.setValue(2)}
                         >
                             Process
                         </Grid>
@@ -210,21 +232,30 @@ export default function Footer(props) {
                 </Grid>
             </Grid>
         </Hidden>
+
         <img
             alt={"black decorative slash"}
             src={"/assets/footerAdornment.svg"}
             className={classes.adornment}
         />
-        <Grid container justifyContent={"flex-end"}
-              spacing={2} className={classes.socialContainer}>
+
+        <Grid
+            container
+            justifyContent={"flex-end"}
+            spacing={2}
+            className={classes.socialContainer}
+        >
             <Grid
-                item component={"a"}
+                item
+                component={"a"}
                 href={"https://www.facebook.com/shahrooz.sabet/"}
                 rel={"noopener noreferrer"}
                 target={"_blank"}
             >
-                <img alt={"Facebook Logo"} src={"/assets/facebook.svg"}
-                     className={classes.icon}
+                <img
+                    alt={"Facebook Logo"}
+                    src={"/assets/facebook.svg"}
+                    className={classes.icon}
                 />
             </Grid>
             <Grid
@@ -233,20 +264,25 @@ export default function Footer(props) {
                 rel={"noopener noreferrer"}
                 target={"_blank"}
             >
-                <img alt={"Linkedin Logo"} src={"/assets/linkedin.svg"}
-                     className={classes.icon}
+                <img
+                    alt={"Linkedin Logo"}
+                    src={"/assets/linkedin.svg"}
+                    className={classes.icon}
                 />
             </Grid>
             <Grid
-                item component={"a"}
+                item
+                component={"a"}
                 href={"https://www.instagram.com/shahrooz_sabet/"}
                 rel={"noopener noreferrer"}
                 target={"_blank"}
             >
-                <img alt={"Instagram Logo"} src={"/assets/instagram.svg"}
-                     className={classes.icon}
+                <img
+                    alt={"Instagram Logo"}
+                    src={"/assets/instagram.svg"}
+                    className={classes.icon}
                 />
             </Grid>
         </Grid>
-    </footer>)
+    </footer>);
 }
